8b106caba5d5ad2aa628a9e3bcda388b4d9dcfa7
Edwin Pers <pers.edwin@gmail.com>
2025-08-25 22:37:06 -0400

    rbac

---
+++ src/internal/controller/eevee/natscluster_controller.go
@@ -0,0 +1,8 @@
+package controller
+
+// +kubebuilder:rbac:groups=*,resources=events,verbs=create;patch
+// +kubebuilder:rbac:groups=*,resources=pods,verbs=get;list;watch
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=eevee.bot,resources=natsclusters,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=eevee.bot,resources=natsclusters/finalizers,verbs=update
+// +kubebuilder:rbac:groups=eevee.bot,resources=natsclusters/status,verbs=get;update;patch
