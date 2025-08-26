c3bbe2075e407a300a57493f0a16511a61722164
Edwin Pers <pers.edwin@gmail.com>
2025-08-25 21:04:12 -0400

    fixes to rbac and other things

--- src/internal/controller/eevee/connectorirc_controller.go
+++ src/internal/controller/eevee/connectorirc_controller.go
@@ -48,13 +48,13 @@ type ConnectorIrcReconciler struct {
 // when the command <make manifests> is executed.
 // To know more about markers see: https://book.kubebuilder.io/reference/markers.html
 
-// +kubebuilder:rbac:groups=eevee.bot,resources=connectorircs,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=eevee.bot,resources=connectorircs/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=eevee.bot,resources=connectorircs/finalizers,verbs=update
-// +kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+// +kubebuilder:rbac:groups=*,resources=events,verbs=create;patch
+// +kubebuilder:rbac:groups=*,resources=pods,verbs=get;list;watch
+// +kubebuilder:rbac:groups=*,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
-// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=eevee.bot,resources=connectorircs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=eevee.bot,resources=connectorircs/finalizers,verbs=update
+// +kubebuilder:rbac:groups=eevee.bot,resources=connectorircs/status,verbs=get;update;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
