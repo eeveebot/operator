ee9ca38eec90a9eb07d007647143749e8afd1279
Edwin Pers <pers.edwin@gmail.com>
2025-08-27 19:27:20 -0400

    rbac (working!)

--- src/internal/controller/eevee/natscluster_controller.go
+++ src/internal/controller/eevee/natscluster_controller.go
@@ -6,7 +6,9 @@ package controller
 // +kubebuilder:rbac:groups=*,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=*,resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=*,resources=services,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=monitoring.coreos.com,resources=podmonitors,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=eevee.bot,resources=natsclusters,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=eevee.bot,resources=natsclusters/finalizers,verbs=update
 // +kubebuilder:rbac:groups=eevee.bot,resources=natsclusters/status,verbs=get;update;patch
