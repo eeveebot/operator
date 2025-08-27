3849c21b42da23540109af263a1724ef95642602
Edwin Pers <pers.edwin@gmail.com>
2025-08-27 16:27:06 -0400

    rbac for serviceaccounts for nats

--- src/internal/controller/eevee/natscluster_controller.go
+++ src/internal/controller/eevee/natscluster_controller.go
@@ -2,6 +2,7 @@ package controller
 
 // +kubebuilder:rbac:groups=*,resources=events,verbs=create;patch
 // +kubebuilder:rbac:groups=*,resources=pods,verbs=get;list;watch
+// +kubebuilder:rbac:groups=*,resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=eevee.bot,resources=natsclusters,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=eevee.bot,resources=natsclusters/finalizers,verbs=update
