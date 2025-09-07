977ae4ae99a063a93418fae44e7fd4d4f611d248
Edwin Pers <pers.edwin@gmail.com>
2025-09-06 20:17:36 -0400

    re-namespace under spec

--- src/api/eevee/v1alpha1/connectorirc_types.go
+++ src/api/eevee/v1alpha1/connectorirc_types.go
@@ -71,27 +71,11 @@ type PostConnectRawSpec struct {
 	Raw      string `json:"raw,omitempty"`
 }
 
-// ConnectorIrcStatus defines the observed state of ConnectorIrc
-type ConnectorIrcStatus struct {
-	// Represents the observations of a ConnectorIrc's current state.
-	// ConnectorIrc.status.conditions.type are: "Available", "Progressing", and "Degraded"
-	// ConnectorIrc.status.conditions.status are one of True, False, Unknown.
-	// ConnectorIrc.status.conditions.reason the value should be a CamelCase string and producers of specific
-	// condition types may define expected values and meanings for this field, and whether the values
-	// are considered a guaranteed API.
-	// ConnectorIrc.status.conditions.Message is a human readable message indicating details about the transition.
-	// For further information see: https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
-	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
-}
-
-// +kubebuilder:object:root=true
-// +kubebuilder:subresource:status
-
-// ConnectorIrc is the Schema for the connectorircs API
-type ConnectorIrc struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
+// ConnectorIrcSpec defines the desired state of ConnectorIrc
+type ConnectorIrcSpec struct {
+	// Name defines the name of this ConnectorIRC
+	// +kubebuilder:default="eevee-connector-irc"
+	Name string `json:"name,omitempty"`
 
 	// Size defines the number of ConnectorIrc instances
 	// The following markers will use OpenAPI v3 schema to validate the value
@@ -127,8 +111,32 @@ type ConnectorIrc struct {
 	// Metrics defines to enable metrics or not
 	// +kubebuilder:default=false
 	Metrics bool `json:"metrics,omitempty"`
+}
 
-	Status ConnectorIrcStatus `json:"status,omitempty"`
+// ConnectorIrcStatus defines the observed state of ConnectorIrc
+type ConnectorIrcStatus struct {
+	// Represents the observations of a ConnectorIrc's current state.
+	// ConnectorIrc.status.conditions.type are: "Available", "Progressing", and "Degraded"
+	// ConnectorIrc.status.conditions.status are one of True, False, Unknown.
+	// ConnectorIrc.status.conditions.reason the value should be a CamelCase string and producers of specific
+	// condition types may define expected values and meanings for this field, and whether the values
+	// are considered a guaranteed API.
+	// ConnectorIrc.status.conditions.Message is a human readable message indicating details about the transition.
+	// For further information see: https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// ConnectorIrc is the Schema for the connectorircs API
+type ConnectorIrc struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ConnectorIrcSpec   `json:"spec"`
+	Status ConnectorIrcStatus `json:"status"`
 }
 
 // +kubebuilder:object:root=true
